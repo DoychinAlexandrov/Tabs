7ea136f6a5c545b5531c341887af3eed96884244
DoychinAlexandrov <doychin_alexandrov@abv.bg>
2023-05-20 14:50:20 +0300

    add btn-container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import JobInfo from "./JobInfo";
+import BtnContainer from "./BtnContainer";
 const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
@@ -24,7 +25,8 @@ const App = () => {
     }
     return (
         <section className="jobs-center">
-            <div className="loading"></div>
+            <BtnContainer jobs={jobs} />
+
             <JobInfo jobs={jobs} />
         </section>
     );
